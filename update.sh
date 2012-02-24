b4fdd93cdaf20c65ae79ea2f23e8fbd6707c0703
Corvin89 <corvin8989@gmail.com>
2012-02-24 12:04:23 +0200

    Testderive

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!sh
+#git stash
+#git pull origin master
+#mysql -uadmin -padmin -e "drop database student;"
+#mysql -uadmin -padmin -e "create database student;"
+#mysql -uadmin -padmin student < backup/database.sql
+#mysql -uadmin -padmin -e "UPDATE wp_options SET option_value = 'http://student.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" student
+#git stash pop
+
